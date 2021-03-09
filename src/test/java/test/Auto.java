4604f5a9ed20ec3ee20924029406c945b9b04484
vseguro <79714681+vseguro@users.noreply.github.com>
2021-03-09 11:46:14 -0500

    Taller 2

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -4,19 +4,16 @@ public class Auto {
 	String modelo;
 	int precio;
 	Asiento asientos[];
-	Asiento registroAsiento[];
-	Motor registroMotor[];
-	Auto registroAuto[];
-	String marca;
 	Motor motor;
+	String marca;
 	int registro;
 	static int cantidadCreados;
 
-	int cantidadAsientos() {
+	int cantidadAsientos() {	
 		int cont = 0;
-		for (int i = 0; i < asientos.length; i++) {
-			if (asientos[i] != null) {
-				cont = cont + 1;
+		for (int i = 0; i < asientos.length;i++) {
+			if (asientos[i]!=null) {
+				cont=cont+1;
 			}
 		}
 		return cont;
@@ -25,22 +22,26 @@ public class Auto {
 
 	
 	String verificarIntegridad() {
-		int conta=0;
-		for (int i = 0; i < cantidadCreados;i++) {
-			if ((registroAsiento!=null) && (registroMotor!=null) && (registroAuto!=null)){
-				if ((registroAsiento[i].equals(registroMotor[i]))&&(registroAsiento[i].equals(registroAuto[i]))&&
-						(registroAuto[i].equals(registroMotor[i]))){
-					conta=0;
-				} else {
-					conta= conta+1;
-				}
-			}
-			
-			}
-		if (conta==0) {
-			return "Auto original";
-		} else {
-			return "Las piezas no son originales";
+		int conta1=0;
+		int conta2=0;
+		if (motor.registro==registro) {
+			conta1=0;
 		}
+		for (int i = 0; i < asientos.length;i++) {
+			if (asientos[i]!=null) {
+			if ((asientos[i].registro!=(registro))){
+				conta2=conta2+1;
+			}
+		}
+		}
+		if (conta1==0 && conta2==0){
+			return "Auto original";
+			
+		}else {
+			return "Las piezas no son originales";
+		
+		}
+				
+
 	}
 }
--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -13,9 +13,7 @@ public class Motor {
 		if ((tipo.equals("electrico"))||(tipo.equals("gasolina"))) {
 			this.tipo=tipo;
 		}
-		else {
-			tipo=tipo;
-		}
+
 	}
 
 }
